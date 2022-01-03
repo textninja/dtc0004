df26131063f1003204af879845266e6f7df55545
Joe Taylor <joe@textninja.net>
2022-01-02 22:35:13 -0800

    Added sound playback

--- practice/playsound/App.js
+++ practice/playsound/App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import { Audio } from 'expo-av';
+
+async function playSound() {
+  const ding = await Audio.Sound.createAsync(
+    require('./assets/ding.wav')
+  );
+
+  await ding.sound.playAsync();
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ backgroundColor: "#000", padding: 50, flex: 1, justifyContent: "center" }}>
+      <TouchableHighlight style={styles.button} onPress={playSound}>
+        <Text style={{ color: "#fff" }}>Play a sound</Text>
+      </TouchableHighlight>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  button: {
+    padding: 20,
+    backgroundColor: "#0088cc",
+    alignContent: "center",
+    display: "flex",
+    alignItems: "center",
+  }
+})
