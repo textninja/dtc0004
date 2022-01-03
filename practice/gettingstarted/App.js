cd7a6da7ac0371b17303753f228c5a079a390d59
Joe Taylor <joe@textninja.net>
2022-01-02 16:00:26 -0800

    Updated app to use text input and say hello

--- practice/gettingstarted/App.js
+++ practice/gettingstarted/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+const { useState } = React;
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+const defaultName = "Joe";
+
+const reallyBigText = {
+  fontSize: 40
+};
+
+
+const bigText = {
+  fontSize: 20
+}
 
 const App = () => {
+
+  const [name, setName] = useState(defaultName);
+
+  const displayName = name ? name : "friend";
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 20 }}>
-        Try editing me!
-      </Text>
+      <Text style={reallyBigText}>Hello, {displayName}!</Text>
+
+      <Text style={{...bigText, marginTop: 40, marginBottom: 20}}>Try changing your name.</Text>
+      <TextInput
+        style={{...bigText, borderColor: "black", borderWidth: 1, padding: 10, width: 200 }}
+        defaultValue={defaultName}
+        onChangeText={(text) => setName(text)} />
     </View>
   );
 }
