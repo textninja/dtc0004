1b225ec41b30a649cf1c0a9ee5f3a942261376da
Joe Taylor <joe@textninja.net>
2022-01-02 18:35:46 -0800

    Updated flatlist example to use sectionlist

--- practice/gettingstarted/examples/5.js
+++ practice/gettingstarted/examples/5.js
@@ -1,25 +1,47 @@
 import React, { useState } from 'react';
-import { FlatList, Text, StyleSheet } from 'react-native';
+import { View, SectionList, Text, StyleSheet } from 'react-native';
 
 const names = "Liam Olivia Noah Emma Oliver Ava Elijah Charlotte William Sophia James Amelia Benjamin Isabella Lucas Mia Henry Evelyn Alexander Harper"
-    .split(" ")
-    .map(i => ({ key: i }));
+    .split(" ");
+
+const firstLetters = [...names.reduce((letters, name) => {
+    letters.add(name[0]);
+    return letters;
+}, new Set)].sort();
+
+const sections = firstLetters.map(letter => (
+    {
+        title: letter,
+        data: names.filter(n => n[0] == letter)
+    }
+));
 
 const App = () => {
-    const [people, _] = useState(names);
-
-    return <FlatList
-        data={names}
-        renderItem={({item}) => <Text style={style.flatListItem}>{item.key}</Text>}
-    />
+    return (
+        <View>
+            <SectionList
+                style={{marginTop: 40}}
+                sections={sections}
+                renderItem={({item}) => <Text style={style.listItem}>{item}</Text>}
+                renderSectionHeader={({section}) => <Text style={style.listSectionHeader}>{section.title}</Text>}
+                keyExtractor={(item, index) => index}
+            />
+        </View>
+    );
 };
 
 
 const style = StyleSheet.create({
-    flatListItem: {
-        padding: 20,
-        fontSize: 20,
-        textAlign: "center"
+    listItem: {
+        padding: 10,
+        fontSize: 18,
+        height: 44
+    },
+
+    listSectionHeader: {
+        fontSize: 18,
+        paddingLeft: 10,
+        backgroundColor: "#ddd"
     }
 })
 
