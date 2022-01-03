248e59d75954393876e7cf151d0a2e49bb58ac17
Joe Taylor <joe@textninja.net>
2022-01-02 17:19:20 -0800

    Followed hungry cats example

--- practice/gettingstarted/App.js
+++ practice/gettingstarted/App.js
@@ -1,38 +1,34 @@
 import React from 'react';
 const { useState } = React;
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-
+import {
+  Text, Button, View,
+  StyleSheet
+} from 'react-native';
 
 const Cat = () => {
-  return (
-    <View>
-        <Text>This is a cat.</Text>
-    </View>
-  );
-}
+    let [isHungry, setHungry] = useState(Math.random()>0.5);
 
-const CatCafe = ({ cats=10 }) => {
-  return (
-    <View>
-      <Text>Welcome to the cat cafe!</Text>
-
-      <View style={{ marginTop: 10 }}>
-      {
-        Array(cats).fill(0).map(c => <Cat/>)
-      }
+    return (
+      <View>
+        <Text>I am a cat. I am { isHungry ? "hungry." : "not hungry." }</Text>
+        <Button
+          disabled={!isHungry}
+          onPress={() => setHungry(false)}
+          title="Feed me!" />
       </View>
-    </View>
-  );
-}
-
-const App = () => {
-
-  return (
-    <View style={{flex: 1, justifyContent: "center", marginLeft: 20 }}>
-      <CatCafe cats={5} />
-    </View>
-  );
-
+    );
 };
+
+
+const App = () => (
+  <View style={{ flex: 1, marginTop: 60, marginLeft: 20 }}>
+    <Text style={{ fontSize: 20, marginBottom: 20 }}>Welcome to the cat cafe!</Text>
+    {
+      Array.from({ length: 5 }, () => <Cat/>)
+    }
+  </View>
+);
+
+
 
 export default App;
